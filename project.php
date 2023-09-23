8ee6cb62bcede2447f40b934d71af0984a3f93e9
Sayan <144830101+Novadotgg@users.noreply.github.com>
2023-09-23 09:50:24 +0530

    resolved database connection issues
    now the data directly gets inserted to the database

--- project.php
+++ project.php
@@ -1,51 +1,77 @@
-
+<!DOCTYPE html>
 <html>
-
 <head>
-<title>URL Shortener</title>
+    <title>URL Shortener</title>
 </head>
 <body>
-<center>
-<div style="padding":30px;">
-<h1>Project on URL Shortening</h1>
-<form method="post"action="#">
+    <h1>URL Shortener</h1>
+    <?php
+    // Database configuration
 
+    $db_host = 'localhost';
+    $db_user = 'root';
+    $db_pass = 'Shanks0p';
+    $db_name = 'url_shortener';
 
+    // Establish a database connection
+    $conn = new mysqli($db_host, $db_user, $db_pass, $db_name);
 
-<input type="text" name="input_url" required style="width:500px;padding:10px;font-size:1.5em;">
+    // Check connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
 
+    // Function to generate a random short code
+    function generateShortCode() {
+        $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        $short_code = '';
+        $length = 6; // Change the length as needed
 
+        for ($i = 0; $i < $length; $i++) {
+            $short_code .= $characters[rand(0, strlen($characters) - 1)];
+        }
 
+        return $short_code;
+    }
 
-<input type="submit" name="submit " value="shorten"
-</form>
-</center>
+    // Shorten URL and insert into the database
+    if (isset($_POST['original_url'])) {
+        $original_url = $_POST['original_url'];
+        $short_code = generateShortCode();
+
+        $sql = "INSERT INTO urls (original_url, short_code) VALUES ('$original_url', '$short_code')";
+
+        if ($conn->query($sql) === TRUE) {
+            $shortened_url = $_SERVER['HTTP_HOST'] . '/' . $short_code;
+            echo "<p>Shortened URL: <a href='$shortened_url'>$shortened_url</a></p>";
+        } else {
+            echo "Error: " . $sql . "<br>" . $conn->error;
+        }
+    }
+
+    // Redirect to the original URL
+    if (isset($_GET['code'])) {
+        $short_code = $_GET['code'];
+        $sql = "SELECT original_url FROM urls WHERE short_code = '$short_code'";
+        $result = $conn->query($sql);
+
+        if ($result->num_rows > 0) {
+            $row = $result->fetch_assoc();
+            $original_url = $row['original_url'];
+            header("Location: $original_url");
+            exit;
+        } else {
+            echo "URL not found.";
+        }
+    }
+
+    $conn->close();
+    ?>
+
+    <form method="POST" action="">
+        <input type="url" name="original_url" placeholder="Enter URL to shorten" required>
+        <input type="submit" name="submit" value="Shorten">
+    </form>
 </body>
 </html>
-<?php
-
-
-if(isset($_POST['submit'])){
-
-$conn=mysqli_connect('localhost','root','','short_url_data'); 
-if(!conn){
-echo "connection error";
-exit();
-}
-
-
-$orig_url=$_POST['input_url'];
-$rand=substr(md5(microtime()), rand(0,26),3);
-mysqli_query($conn,"insert into 'short_url_data'('orig_url','unique_exten') values('orig_url','')"); 
-
-
-echo "localhost/a?"."$rand";
-//echo "<a href=http://localhost/a?"."$rand>localhost/a?$rand</a>";
-
-
-}
-?>
-
-
-
 
